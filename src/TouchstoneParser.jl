13472ca69b857d887d53c2b831f0825fc72eb545
Uve Gege <victor97gg@outlook.es>
2025-11-10 22:47:10 +0100

    Prepare registration

--- src/TouchstoneParser.jl
+++ src/TouchstoneParser.jl
@@ -12,30 +12,6 @@ include("./ParserFunctions.jl")
 include("./TouchstoneReader.jl")
 include("./TouchstoneWriter.jl")
 
-
-using PrecompileTools
-
-@setup_workload begin
-    f = [2.0, 22.0]
-    S = [
-        [[0.95 * cis(deg2rad(-26)), 3.57 * cis(deg2rad(157))];;
-        [0.04 * cis(deg2rad(76 )), 0.66 *cis(deg2rad(-14))]], 
-        [[0.60 * cis(deg2rad(-144 )), 1.30 * cis(deg2rad(40))];;
-        [0.14 * cis(deg2rad(40)), 0.56 * cis(deg2rad(-85))]]
-    ]
-
-    z0 = [50, 25.0]
-    noise_f = [4.0, 18.0]
-    noise_data = [NoiseData(0.7, 0.64 * cis(deg2rad(69)), 19), NoiseData(0.7, 0.46 * cis(deg2rad(-33)), 20)]
-
-    @compile_workload begin
-        TouchstoneParser.write_touchstone("Example.s2p", f, S, z0; 
-        version = "1.0", noise_data = noise_data, noise_f = noise_f, twoportorder = "21_12")
-        TouchstoneParser.write_touchstone("Example.ts", f, S, z0; 
-        version = "2.1", noise_data = noise_data, noise_f = noise_f, twoportorder = "21_12")
-    end
-end
-
 end
 
 
